a3b22efb2f88060d20993d03e755a1f4bd0aef4f
t-guoyu.dong <407479757@qq.com>
2022-03-14 00:48:43 +0800

    更新错误

--- src/main/java/com/agoni/dgy/controller/RoleController.java
+++ src/main/java/com/agoni/dgy/controller/RoleController.java
@@ -5,6 +5,8 @@ import com.agoni.dgy.model.po.Role;
 import com.agoni.dgy.service.RoleService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,18 +24,21 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/dgy/role")
+@Api(tags="角色")
 public class RoleController {
 
     @Autowired
     private RoleService roleService;
 
-    @PostMapping("/selectPage")
-    public Page selectPage(@RequestBody Page page) {
+    @PostMapping("/Page")
+    @ApiOperation("分页获取角色信息")
+    public ResponseEntity<Page> Page(@RequestBody Page page) {
         Page res = roleService.page(page,new QueryWrapper<>());
-        return res;
+        return new ResponseEntity<>(res, HttpStatus.OK);
     }
 
-    @GetMapping("/list")
+    @GetMapping
+    @ApiOperation("获取角色信息")
     public ResponseEntity<List> list(){
         List<Role> list = roleService.list();
         return new ResponseEntity<>(list, HttpStatus.OK);
--- src/main/java/com/agoni/security/controller/WeChatController.java
+++ src/main/java/com/agoni/security/controller/WeChatController.java
@@ -1,21 +1,28 @@
 package com.agoni.security.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.api.WxConsts;
+import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
+import me.chanjar.weixin.common.bean.oauth2.WxOAuth2AccessToken;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpService;
 import me.chanjar.weixin.mp.bean.result.WxMpQrCodeTicket;
+import me.chanjar.weixin.mp.bean.result.WxMpUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.websocket.server.PathParam;
+import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 @RestController
 @RequestMapping("/wechat")
 @Slf4j
+@Api(tags="微信")
 public class WeChatController {
 
     private WxMpService wxMpService;
@@ -27,31 +34,35 @@ public class WeChatController {
     }
 
     @GetMapping("/auth_url")
+    @ApiOperation("用户跳转微信授权")
     public String getAuthUrl() throws UnsupportedEncodingException {
         String redirectUrl ="http://225h70r656.wicp.vip/wechat/invoke";
-        String encode = URLEncoder.encode(redirectUrl, "UTF-8");
-        return wxMpService.getOAuth2Service().buildAuthorizationUrl(encode, WxConsts.OAuth2Scope.SNSAPI_USERINFO, null);
+        return wxMpService.getOAuth2Service().buildAuthorizationUrl(redirectUrl, WxConsts.OAuth2Scope.SNSAPI_USERINFO, null);
     }
 
     @ResponseBody
     @GetMapping("invoke")
+    @ApiOperation("微信用户授权后回调接口")
     public Object weChatInvoke(HttpServletRequest httpServletRequest) {
         String code = httpServletRequest.getParameter("code");
-        String state = httpServletRequest.getParameter("state");
         log.info("code:"+code);
-        log.info("state"+state);
-
-        return code+state;
+        try {
+            WxOAuth2AccessToken wxOAuth2AccessToken = wxMpService.getOAuth2Service().getAccessToken(code);
+            WxOAuth2UserInfo userInfo = wxMpService.getOAuth2Service().getUserInfo(wxOAuth2AccessToken, null);
+            return userInfo;
+        } catch (WxErrorException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @ResponseBody
     @GetMapping
+    @ApiOperation("生成一个二维码")
     public Object test() throws WxErrorException {
-        // 临时ticket
-        //WxMpQrCodeTicket ticket = wxMpService.getQrcodeService().qrCodeCreateTmpTicket(scene, expire_seconds);
         WxMpQrCodeTicket ticket = wxMpService.getQrcodeService().qrCodeCreateTmpTicket(123, 123455);
-        String url = wxMpService.getQrcodeService().qrCodePictureUrl(ticket.getTicket());
-        return url;
+        File file = wxMpService.getQrcodeService().qrCodePicture(ticket);
+        return file;
     }
 
 }
